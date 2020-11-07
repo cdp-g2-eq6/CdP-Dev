9367a6f2db2c9939c58bba7499f09e4de4ff9893
N3ROO <swellchaser33@gmail.com>
2020-11-07 17:07:53 +0100

    fixed server

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const DATABASE_URL = process.env.DATABASE_URL || 'localhost';
 mongoose.connect(`mongodb://${DATABASE_URL}/cdp-website`, {useNewUrlParser: true});
