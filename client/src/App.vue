8aaee130c95a11bcff0081be6cfa39a60ddaf5e5
N3ROO <swellchaser33@gmail.com>
2020-11-09 13:55:42 +0100

    added navbar to homepage (task 48 actually)

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
+    <Navbar></Navbar>
+
     <h1> Homepage </h1>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar';
 
 export default {
   name: 'App',
   components: {
+    Navbar,
   },
 };
 </script>
