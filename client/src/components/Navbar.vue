b61f54016df55a6f4ac0487ba5b5fa72cac25e20
N3ROO <swellchaser33@gmail.com>
2020-11-09 17:33:46 +0100

    removed useless features of Navbar, fixed height

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,63 +1,45 @@
 <template>
-  <div class="sidebar-page">
-    <section class="sidebar-layout">
-      <b-sidebar
-        position="static"
-        :mobile="mobile"
-        :expand-on-hover="expandOnHover"
-        :reduce="reduce"
-        :fullheight="fullheight"
-        :overlay="overlay"
-        type="is-light"
-        open
-      >
-        <div class="p-1">
-          <div class="block">
-            <img
-              :src="logo"
-              alt="Lightweight UI components for Vue.js based on Bulma"
-            />
-          </div>
-          <b-menu class="is-custom-mobile">
-            <b-menu-list label="Menu">
-              <b-menu-item
-                pack="fas"
-                icon="list"
-                size="is-small"
-                label="Backlog"
-              ></b-menu-item>
-              <b-menu-item
-                pack="fas"
-                icon="tasks"
-                size="is-small"
-                label="Tâches"
-              ></b-menu-item>
-              <b-menu-item
-                active
-                expanded
-                pack="fas"
-                icon="running"
-                label="Sprints"
-              >
-                <b-menu-item
-                  pack="fas"
-                  icon="plus"
-                  label="Ajouter un sprint"
-                ></b-menu-item>
+  <section>
+    <!-- Here go the navbar settings -->
+    <b-sidebar
+      type="is-light"
+      :fullheight="fullheight"
+      :overlay="overlay"
+      open
+    >
+      <div class="p-1">
+        <!-- Logo -->
+        <img :src="logo" alt="logo"/>
+
+        <!-- Menu -->
+        <b-menu>
+          <b-menu-list label="Menu">
+            <b-menu-item pack="fas" icon="list" label="Backlog"></b-menu-item>
+            <b-menu-item pack="fas" icon="tasks" label="Tâches"></b-menu-item>
+            <b-menu-item pack="fas" icon="running">
+              <template slot="label" slot-scope="props">
+                Sprints
+                <b-icon class="is-pulled-right"
+                        :icon="props.expanded ? 'caret-down' : 'caret-up'">
+                </b-icon>
+              </template>
+
+              <!-- Here go all the sprints -->
+              <b-menu-item pack="fas" icon="plus" label="Ajouter un sprint">
               </b-menu-item>
-            </b-menu-list>
-            <b-menu-list label="Actions">
-              <b-menu-item
-                pack="fas"
-                icon="edit"
-                label="Activer l'édition (todo)">
-              </b-menu-item>
-            </b-menu-list>
-          </b-menu>
-        </div>
-      </b-sidebar>
-    </section>
-  </div>
+            </b-menu-item>
+          </b-menu-list>
+
+        <!-- Actions -->
+        <b-menu-list label="Actions">
+          <b-menu-item pack="fas" icon="edit" label="Activer l'édition (todo)">
+          </b-menu-item>
+        </b-menu-list>
+        </b-menu>
+      </div>
+
+    </b-sidebar>
+  </section>
 </template>
 
 <script>
@@ -65,96 +47,15 @@ export default {
   data() {
     return {
       logo: 'https://via.placeholder.com/250x150',
-      expandOnHover: false,
-      mobile: 'reduce', // reduce, hide or fullwidth
-      reduce: false,
       fullheight: true,
-      overlay: true,
+      overlay: false,
     };
   },
 };
 </script>
 
-<style lang="scss">
-
+<style>
 .p-1 {
   padding: 1em;
-}
-
-.sidebar-page {
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  min-height: 100%;
-  // min-height: 100vh;
-  .sidebar-layout {
-    display: flex;
-    flex-direction: row;
-    min-height: 100%;
-    // min-height: 100vh;
-  }
-}
-
-@media screen and (max-width: 1023px) {
-  .b-sidebar {
-    .sidebar-content {
-      &.is-mini-mobile {
-        &:not(.is-mini-expand),
-        &.is-mini-expand:not(:hover) {
-          .menu-list {
-            li {
-              a {
-                span:nth-child(2) {
-                  display: none;
-                }
-              }
-              ul {
-                padding-left: 0;
-                li {
-                  a {
-                    display: inline-block;
-                  }
-                }
-              }
-            }
-          }
-          .menu-label:not(:last-child) {
-            margin-bottom: 0;
-          }
-        }
-      }
-    }
-  }
-}
-@media screen and (min-width: 1024px) {
-  .b-sidebar {
-    .sidebar-content {
-      &.is-mini {
-        &:not(.is-mini-expand),
-        &.is-mini-expand:not(:hover) {
-          .menu-list {
-            li {
-              a {
-                span:nth-child(2) {
-                  display: none;
-                }
-              }
-              ul {
-                padding-left: 0;
-                li {
-                  a {
-                    display: inline-block;
-                  }
-                }
-              }
-            }
-          }
-          .menu-label:not(:last-child) {
-            margin-bottom: 0;
-          }
-        }
-      }
-    }
-  }
 }
 </style>
