25de360f687a8b1e730092f5ca58050645e8bbbb
Nathan Lesné <nathan.lesne@etu.u-bordeaux.fr>
2020-11-23 16:32:31 +0100

    Created test BD model

---
+++ server/src/models/tests.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const autoIncrement = require('mongoose-plugin-autoinc').autoIncrement;
+
+const TestSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  linkedTask: {
+    type: Number,
+    required: true,
+  },
+  statusHistory: [{
+    date: Date,
+    passed: Boolean,
+  }],
+});
+
+TestSchema.plugin(autoIncrement, 'Test');
+const Test = mongoose.model('Test', TestSchema);
+module.exports = Test;
