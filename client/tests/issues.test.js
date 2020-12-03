ad7dcb9356c122dc8722adbfacc6030dcba8d7f7
N3ROO <swellchaser33@gmail.com>
2020-12-03 15:59:23 +0100

    added more debug

--- client/tests/issues.test.js
+++ client/tests/issues.test.js
@@ -1,7 +1,6 @@
 const {Builder, Capabilities} = require('selenium-webdriver');
 const {click} = require('./selenium_utils');
 
-const url = 'http://' + (process.env.HOSTNAME || 'localhost') + ':8080';
 const TIMEOUT = 10000;
 let driver;
 const chromeCapabilities = Capabilities.chrome();
@@ -21,7 +20,6 @@ describe('Issues test', () => {
         .build();
 
     expect(driver).toBeDefined();*/
-    console.log('BEFORE ALL CALLEDDDDDDDDDDDDDDDDDDD');
   });
 
   afterAll(async () => {
@@ -29,22 +27,28 @@ describe('Issues test', () => {
   });
 
   it('The backlog link in the navbar is active once clicked', async () => {
+    console.log('1');
     driver = await new Builder()
         .forBrowser('chrome')
         .withCapabilities(chromeCapabilities)
         .build();
 
+    console.log('2');
     // Go to the page
-    await driver.get(url);
+    await driver.get('http://localhost:8080');
     // await waitForPageToBeLoaded(driver);
-
+    console.log('3');
     // Click the navbar link
     let backlogLink = await driver.findElement({id: 'backlog-link'});
+    console.log('4');
     await click(driver, backlogLink);
+    console.log('5');
 
     // Check its classes
     backlogLink = driver.findElement({id: 'backlog-link'});
+    console.log('6');
     const clazz = await backlogLink.getAttribute('class');
+    console.log('7');
     expect(clazz).toBe('is-active is-expanded');
   }, TIMEOUT);
 });
