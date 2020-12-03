4e0b0b2f3b80eee1cf157579a41141c2a6cefdfd
N3ROO <swellchaser33@gmail.com>
2020-12-03 16:18:03 +0100

    last try

--- client/tests/issues.test.js
+++ client/tests/issues.test.js
@@ -1,18 +1,20 @@
 const chrome = require('selenium-webdriver/chrome');
 const {Builder} = require('selenium-webdriver');
-const {click} = require('./selenium_utils');
-
-const TIMEOUT = 10000;
-let driver;
+const {click, waitForPageToBeLoaded} = require('./selenium_utils');
 
 describe('Issues test', () => {
+  const TIMEOUT = 10000;
+  let driver;
+
   beforeAll(async () => {
     driver = await (new Builder()
         .forBrowser('chrome')
         .setChromeOptions(new chrome.Options().headless())
         .build());
 
-    expect(driver).toBeDefined();
+    it('should load the driver correctly', () => {
+      expect(driver).toBeDefined();
+    });
   });
 
   afterAll(async () => {
@@ -20,22 +22,17 @@ describe('Issues test', () => {
   });
 
   it('The backlog link in the navbar is active once clicked', async () => {
-    console.log('2');
     // Go to the page
     await driver.get('http://localhost:8080');
-    // await waitForPageToBeLoaded(driver);
-    console.log('3');
+    await waitForPageToBeLoaded(driver);
+
     // Click the navbar link
     let backlogLink = await driver.findElement({id: 'backlog-link'});
-    console.log('4');
     await click(driver, backlogLink);
-    console.log('5');
 
     // Check its classes
     backlogLink = driver.findElement({id: 'backlog-link'});
-    console.log('6');
     const clazz = await backlogLink.getAttribute('class');
-    console.log('7');
     expect(clazz).toBe('is-active is-expanded');
   }, TIMEOUT);
 });
