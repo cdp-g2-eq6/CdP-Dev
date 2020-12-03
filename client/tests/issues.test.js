fd0b347d5dfe5df1c4f89d7418c11763a0bfa40e
N3ROO <swellchaser33@gmail.com>
2020-12-03 15:49:54 +0100

    increased timeout

--- client/tests/issues.test.js
+++ client/tests/issues.test.js
@@ -2,7 +2,7 @@ const {Builder, Capabilities} = require('selenium-webdriver');
 const {click, waitForPageToBeLoaded} = require('./selenium_utils');
 
 const url = 'http://' + (process.env.HOSTNAME || 'localhost') + ':8080';
-const TIMEOUT = 5000;
+const TIMEOUT = 10000;
 let driver;
 const chromeCapabilities = Capabilities.chrome();
 const chromeOptions = {
@@ -13,16 +13,17 @@ const chromeOptions = {
 chromeCapabilities.set('goog:chromeOptions', chromeOptions);
 
 describe('Issues test', () => {
-  beforeAll(async function() {
+  beforeAll(async () => {
     driver = await new Builder()
         .forBrowser('chrome')
         .withCapabilities(chromeCapabilities)
         .build();
 
     expect(driver).toBeDefined();
+    console.log("BEFORE ALL CALLEDDDDDDDDDDDDDDDDDDD");
   });
 
-  afterAll(async function() {
+  afterAll(async () => {
     await driver.quit();
   });
 
